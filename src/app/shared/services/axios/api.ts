ac7d6a7db9f48036e03be055e60fcc13edc22213
Felipe <felipesullivan50@gmail.com>
2023-11-18 18:22:19 -0300

    update FAQApi baseURL

--- src/app/shared/services/axios/api.ts
+++ src/app/shared/services/axios/api.ts
@@ -1,12 +1,9 @@
 import axios, { AxiosInstance } from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
-const FAQApiBaseURL = process.env.NEXT_PUBLIC_FAQ_API_BASE_URL;
 
 export const api: AxiosInstance = axios.create({
   baseURL
 });
 
-export const FAQApi: AxiosInstance = axios.create({
-  baseURL: FAQApiBaseURL
-});
+export const FAQApi: AxiosInstance = axios.create({});
