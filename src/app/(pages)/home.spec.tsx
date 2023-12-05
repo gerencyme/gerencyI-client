91254a1faf343d61e980e2fe80764b1dc312725e
Moisés Santana <moisaant@gmail.com>
2023-12-05 17:36:30 -0300

    test: remove old tests and add todo tests

---
+++ src/app/(pages)/home.spec.tsx
@@ -0,0 +1,6 @@
+describe('Home Page Integration', () => {
+  test.todo('should render FAQ titles');
+  test.todo('should render FAQ description when click on title');
+  test.todo('should not render FAQ description when click on title');
+  test.todo('should render only 5 FAQ items');
+});
