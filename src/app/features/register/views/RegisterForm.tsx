7789b8d8d53aeffb650da1d4f6cfe16bcc564a18
Felipe <felipesullivan50@gmail.com>
2023-12-16 20:30:22 -0300

    fix: corrige weight para weight

--- src/app/features/register/views/RegisterForm.tsx
+++ src/app/features/register/views/RegisterForm.tsx
@@ -156,7 +156,7 @@ export function RegisterForm({ handleForgetPassword }: RegisterFormProps) {
             text="Esquceu sua senha?"
             size="sm"
             color="white"
-            weigth="bold"
+            weight="bold"
             onClick={handleForgetPassword}
             className="cursor-pointer"
           />
