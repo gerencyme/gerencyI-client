49deb666fd71a3ea8728edd848abed7742938b48
Felipe <felipesullivan50@gmail.com>
2023-12-16 20:24:24 -0300

    fix: corrige importação da typagem AuthRequest

--- src/app/features/auth/controller/index.ts
+++ src/app/features/auth/controller/index.ts
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { TAuthSubmitSchema, authSubmitSchema } from '../AuthUtils';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { UserAuth, auth } from '../services';
+import { auth } from '../services';
 import { maskCpfOrCnpj } from '~/src/app/shared/utils/transformers';
 import { useTimeout } from '~/src/app/shared/hooks/useTimeout';
 import { useCookie } from '~/src/app/shared/hooks/useCookies';
@@ -12,6 +12,7 @@ import { APP_ROUTES } from '~/src/app/shared/utils/app-routes';
 import { tokenUserKey } from '~/src/app/shared/utils/constants/cookies';
 import { sessionUserLocalStorage } from '~/src/app/shared/utils/constants/userLocalStorage';
 import { useLocalStorage } from '~/src/app/shared/hooks/useLocalStorage';
+import { AuthRequest } from '~/src/app/shared/types/requests/AuthRequest';
 
 export const useAuthController = () => {
   const { push } = useRouter();
@@ -39,7 +40,7 @@ export const useAuthController = () => {
   const formattedCnpj = maskCpfOrCnpj(cnpj || '');
 
   const onSubmit = async (data: TAuthSubmitSchema) => {
-    await auth(data as UserAuth, setErrorResolver).then((resp) => {
+    await auth(data as AuthRequest, setErrorResolver).then((resp) => {
       if (resp !== 'usuario não existe') {
         // validar statuscode
         createSession({
