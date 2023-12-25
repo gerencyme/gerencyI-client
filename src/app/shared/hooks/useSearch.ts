dbc639304903e6845eeaa15dc3c90a27fa84c3ea
Felipe <felipesullivan50@gmail.com>
2023-12-25 16:45:10 -0300

    fix: adiciona estado de load e remove useMemo

--- src/app/shared/hooks/useSearch.ts
+++ src/app/shared/hooks/useSearch.ts
@@ -1,4 +1,5 @@
-import { ChangeEvent, useMemo, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { useTimeout } from './useTimeout';
 import { ProductCard } from '../types/ProductCard';
 
 /**
@@ -6,19 +7,25 @@ import { ProductCard } from '../types/ProductCard';
  */
 
 export const useSearch = (data: ProductCard[]) => {
+  const [isTyping, setIsTyping] = useState(false);
   const [search, setSearch] = useState('');
+
+  const situation = isTyping;
+  const updateSituation = () => setIsTyping(false);
+
+  useTimeout(situation, updateSituation, 1500);
 
   const onchange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
 
+    setIsTyping(true);
     setSearch(value.toLowerCase());
   };
 
-  const filteredData = useMemo(() => {
-    return search.length > 0
+  const filteredData =
+    search.length > 0
       ? data.filter((item) => item.productName.toLowerCase().match(new RegExp(search, 'i')))
       : data;
-  }, [data, search]);
 
-  return { onchange, search, filteredData };
+  return { onchange, search, filteredData, isTyping };
 };
